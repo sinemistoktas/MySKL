ff205707c16bd2abf284b3a67a89c4c0a2852d37
Muhammet Eren ozogul <muhammeterenozogul@gmail.com>
2025-01-22 20:57:00 +0100

    rating added

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -1,19 +1,45 @@
+# import mysql.connector
+
+# def get_connection(password, database_name):
+#     try:
+#         connection = mysql.connector.connect(
+#             host="localhost",
+#             user="root",
+#             passwd=password,
+#             allow_local_infile= True,
+#         )
+
+#         if database_name:
+#             connection.database = database_name  # This sets the database for the connection
+
+#         # Set autocommit to True    
+#         connection.autocommit = True
+
+#         print()
+#         return connection
+
+#     except Exception as e:
+#         print(f"General Error: {e}")
+
+
 import mysql.connector
 
-def get_connection(password, database_name):
+def get_connection(password, database_name, new=False):
     try:
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
             passwd=password,
             allow_local_infile= True,
+            autocommit = True # Set autocommit to True    
         )
-
-        if database_name:
+        if new:
+            cursor = connection.cursor()
+            cursor.execute(f"DROP DATABASE IF EXISTS {database_name};")  # Drop database if it already exists
+            cursor.execute(f"CREATE DATABASE {database_name};") # Create the database
+        else:
             connection.database = database_name  # This sets the database for the connection
 
-        # Set autocommit to True    
-        connection.autocommit = True
 
         print()
         return connection
